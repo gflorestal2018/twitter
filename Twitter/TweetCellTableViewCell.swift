e2dd66f204a8e0d062d319afc8a525bd5bda4d8b
gflorestal2018 <77472969+gflorestal2018@users.noreply.github.com>
2022-09-25 12:55:32 -0400

    update

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class TweetCellTableViewCell: UITableViewCell {
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    func setFavorited(_ isFavorited:Bool){
+        favorited = isFavorited
+        if(favorited){
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        } else {
+            favButton.setImage(UIImage(named: "favor-icon-1"), for: UIControl.State.normal)
+        }
+    }
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var tweetContent: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
@@ -18,18 +28,24 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBAction func retweetTweet(_ sender: Any) {
     }
     @IBAction func favoriteTweet(_ sender: Any) {
-    }
-    
-    var favorited:Bool = false
-    var tweetId:Int = -1
-    
-    func setFavorited(_ isFavorited:Bool){
-        favorited = isFavorited
-        if(favorited){
-            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        let toBeFavorited = !favorited
+        if (toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorited(true)
+            }, failure: { (error) in
+                print("Favorite did not succeed \(error)")
+            })
         } else {
-            favButton.setImage(UIImage(named: "favor-icon-1"), for: UIControl.State.normal)        }
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorited(false)
+            }, failure: { (error) in
+                print("unfavorite did not succeed \(error)")
+            })
+        }
     }
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
