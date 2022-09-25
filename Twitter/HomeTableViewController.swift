3018e61e99882802e213ab6ae9e3382d876aa168
gflorestal2018 <77472969+gflorestal2018@users.noreply.github.com>
2022-09-25 12:26:03 -0400

    favorite

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -19,6 +19,8 @@ class HomeTableViewController: UITableViewController {
         loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
        
     }
     
@@ -86,6 +88,8 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        cell.setFavorited(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
         return cell
     }
     
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -12,8 +12,24 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var tweetContent: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var favButton: UIButton!
+    @IBOutlet weak var rtButton: UIButton!
     
+    @IBAction func retweetTweet(_ sender: Any) {
+    }
+    @IBAction func favoriteTweet(_ sender: Any) {
+    }
     
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    func setFavorited(_ isFavorited:Bool){
+        favorited = isFavorited
+        if(favorited){
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        } else {
+            favButton.setImage(UIImage(named: "favor-icon-1"), for: UIControl.State.normal)        }
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
